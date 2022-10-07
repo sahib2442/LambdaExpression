c3ddc401767be175f98fab62703dcb5bc7937f87
sahib2442 <kadersahib1234@gmail.com>
2022-10-07 14:49:50 +0530

    [sahib] Add: List of Person

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LambdaExpressionDemo
+{
+    class Person
+    {
+        public string? SSN;
+        public string? Name;
+        public string? Address;
+        public int? Age;
+        internal bool name;
+        private string v1;
+        private string v2;
+        private string v3;
+        private int v4;
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+
+        }
+        public Person(string? sSN, string? name, string? address, int? age, string v1, string v2, string v3, int v4)
+        {
+            SSN = sSN;
+            Name = name;
+            Address = address;
+            Age = age;
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LambdaExpressionDemo
+{
+    class program
+    {
+        private static readonly List<Person> listPersonsInCity;
+
+        static void Main(string[] args)
+        {
+            // UC 1:create list and insert record;
+            List<Person> list = new List<Person>();
+            AddRecords(listPersonsInCity);
+        }
+        // uc 1
+        private static void AddRecords(List<Person> listPersonsInCity)
+        {
+            listPersonsInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork, NY", 15));
+            listPersonsInCity.Add(new Person("203456877", "SAm", "13 Main Street, Newyork, Ny", 25));
+            listPersonsInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork, NY", 25));
+            listPersonsInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork, NY", 45));
+            listPersonsInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton, DN", 55));
+            listPersonsInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork, NY", 65));
+            listPersonsInCity.Add(new Person("203456882", "Wiston", "1208 Alex St, Newyork, NY", 65));
+            listPersonsInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltinore, NY", 85));
+            listPersonsInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltinore, NY", 95));
+            foreach (Person person in listPersonsInCity)
+            {
+                Console.WriteLine("SSN : {0} Name : {1} Address : {2} Age : {3} ", person.SSN, person.Name, person.Address, person.Age);
+            }
+        }
+    }
+}
+
+       
